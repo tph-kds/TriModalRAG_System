25b4406f2068ec5315f9214d00697725679ec72f
tph-kds <hungcompo123@gmail.com>
2024-10-21 13:53:38 +0700

    fix: resolve some errors

--- tests/test_file.py
+++ tests/test_file.py
@@ -1,6 +1,19 @@
-from src.config_params import   LANGCHAIN_PROJECT
-from dotenv import load_dotenv
-load_dotenv()
-def test_file():
-    
-    assert LANGCHAIN_PROJECT == "trimodal_rag"
+
+from src.trim_rag.exception import MyException
+import sys
+def test_log_message():
+    try: 
+
+        x = 1/0
+
+    except Exception as e:
+
+        my_exception = MyException(
+            error_message = "Failed to run Data Embedding pipeline: " + str(e),
+            error_details = sys,
+        )
+        print(my_exception)
+        assert my_exception is not None 
+        assert my_exception.error_message == "Failed to run Data Embedding pipeline: " + str(e)
+
+
